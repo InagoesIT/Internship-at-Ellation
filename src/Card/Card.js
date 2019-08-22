96c2a9fa0e7e158a0b32077c0493ec2367a8b3ae
Ina Vivdici <ivivdici@Ellations-iMac.local>
2019-08-22 15:46:06 +0300

    added API

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,83 +1,22 @@
 import React from "react";
 import "./Card.css";
 
-function Card() {
+function Card({ title, genres, description, src }) {
   return (
     <div className="grid">
       <div className="card">
-        <img
-          className="card-image"
-          src="https://images-na.ssl-images-amazon.com/images/I/91dDv9WOcFL.jpg"
-          alt="The image of the book Sherlock Holmes"
-        />
+        <img className="card-image" src={src} alt="" />
         <div className="card-content">
           <ul className="all-movie-genres">
-            <li className="movie-genre">Detective</li>
-            <li className="movie-genre">Murder</li>
-            <li className="movie-genre">Mistery</li>
+            {genres.map((genre, id) => (
+              <li key={id} className="movie-genre">
+                {genre}
+              </li>
+            ))}
           </ul>
-          <h1 className="movie-title">"Sherlock Holmes"</h1>
-          <p className="movie-description">
-            Blablaabm gthsfhtg 6ytrfgb ytrfdgfb yrfdgh. ghtrdg .
-          </p>
-          <button class="read-more-button"> READ MORE</button>
-        </div>
-      </div>
-      <div className="card">
-        <img
-          className="card-image"
-          src="https://upload.wikimedia.org/wikipedia/en/4/4a/And_Then_There_Were_None_First_Edition_Cover_1939.jpg"
-          alt="The image of the book And then there were none"
-        />
-        <div className="card-content">
-          <ul className="all-movie-genres">
-            <li className="movie-genre">Detective</li>
-            <li className="movie-genre">Murder</li>
-            <li className="movie-genre">Mistery</li>
-          </ul>
-          <h1 className="movie-title">"And then there were none"</h1>
-          <p className="movie-description">
-            Blablaabm gthsfhtg 6ytrfgb ytrfdgfb yrfdgh. ghtrdg .
-          </p>
-          <button class="read-more-button"> READ MORE</button>
-        </div>
-      </div>
-      <div className="card">
-        <img
-          className="card-image"
-          src="https://images-na.ssl-images-amazon.com/images/I/A15FFg6aNLL.jpg"
-          alt="The image of the book The Da Vinci Code"
-        />
-        <div className="card-content">
-          <ul className="all-movie-genres">
-            <li className="movie-genre">Detective</li>
-            <li className="movie-genre">Murder</li>
-            <li className="movie-genre">Mistery</li>
-          </ul>
-          <h1 className="movie-title">"The Da Vinci Code"</h1>
-          <p className="movie-description">
-            Blablaabm gthsfhtg 6ytrfgb ytrfdgfb yrfdgh. ghtrdg . sgfshdsjh. jhg
-          </p>
-          <button class="read-more-button"> READ MORE</button>
-        </div>
-      </div>
-      <div className="card">
-        <img
-          className="card-image"
-          src="https://images-na.ssl-images-amazon.com/images/I/91C-9nqSkrL.jpg"
-          alt="The image of the book Eeny Meeny"
-        />
-        <div className="card-content">
-          <ul className="all-movie-genres">
-            <li className="movie-genre">Detective</li>
-            <li className="movie-genre">Murder</li>
-            <li className="movie-genre">Mistery</li>
-          </ul>
-          <h1 className="movie-title">"Eeny Meeny"</h1>
-          <p className="movie-description">
-            Blablaabm gthsfhtg 6ytrfgb ytrfdgfb yrfdgh. ghtrdg .
-          </p>
-          <button class="read-more-button"> READ MORE</button>
+          <h1 className="movie-title">{title}</h1>
+          <p className="movie-description">{description}</p>
+          <button className="read-more-button">READ MORE</button>
         </div>
       </div>
     </div>
--- src/Collection/Collection.js
+++ src/Collection/Collection.js
@@ -1,13 +1,47 @@
 import React from "react";
 import Card from "../Card/Card";
 import "./Collection.css";
+import { API_KEY } from "../constants.js";
 
-function Collection() {
-  return (
-    <div className="collection">
-      <Card />
-    </div>
-  );
+class Collection extends React.Component {
+  state = {
+    status: "success",
+    movies: [],
+  };
+  componentDidMount() {
+    fetch("https://api.themoviedb.org/3/discover/movie?api_key=" + API_KEY)
+      .then(response => response.json())
+      .then(response => {
+        this.setState({
+          movies: response.results.map(movie => {
+            return {
+              title: movie.original_title,
+              description: movie.overview,
+              src: "https://image.tmdb.org/t/p/w500" + movie.poster_path,
+              genres: movie.genre_ids,
+            };
+          }),
+        });
+      });
+  }
+  render() {
+    console.log(this.state.movies);
+    return (
+      <div className="collection">
+        {this.state.movies.map((movie, index) => {
+          return (
+            <Card
+              key={index}
+              title="Sherlock Holmes"
+              src="https://images-na.ssl-images-amazon.com/images/I/91dDv9WOcFL.jpg"
+              genres={["detective", "masea", "dusea"]}
+              description="blabndgsh hghd jg hygdn jh gujg "
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default Collection;
